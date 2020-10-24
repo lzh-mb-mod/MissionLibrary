d90c31e40e5bbf69eeb03e8874444d0bee39e718
Li Zhenhuan <lizhenhuan1019@qq.com>
2020-10-24 15:00:19 +0800

    Initial commit

---
+++ source/src/Event/MissionEvent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TaleWorlds.MountAndBlade;
+
+namespace MissionLibrary.Event
+{
+    public class MissionEvent
+    {
+        public static event Action<Agent> MainAgentWillBeChangedToAnotherOne;
+
+        public static event Action<bool> ToggleFreeCamera;
+
+        public delegate void SwitchTeamDelegate();
+
+        public static event SwitchTeamDelegate PreSwitchTeam;
+        public static event SwitchTeamDelegate PostSwitchTeam;
+
+        public static void Clear()
+        {
+            MainAgentWillBeChangedToAnotherOne = null;
+            ToggleFreeCamera = null;
+            PreSwitchTeam = null;
+            PostSwitchTeam = null;
+        }
+
+        public static void OnMainAgentWillBeChangedToAnotherOne(Agent newAgent)
+        {
+            MainAgentWillBeChangedToAnotherOne?.Invoke(newAgent);
+        }
+
+        public static void OnToggleFreeCamera(bool obj)
+        {
+            ToggleFreeCamera?.Invoke(obj);
+        }
+
+        public static void OnPreSwitchTeam()
+        {
+            PreSwitchTeam?.Invoke();
+        }
+
+        public static void OnPostSwitchTeam()
+        {
+            PostSwitchTeam?.Invoke();
+        }
+    }
+}
